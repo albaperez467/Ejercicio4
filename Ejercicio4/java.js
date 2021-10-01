eb359baee6271d91145397d014d56a95df4498f4
albaperez467 <91663288+albaperez467@users.noreply.github.com>
2021-10-01 09:58:11 +0200

    Add files via upload

---
+++ Ejercicio4/java.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<html>
+  <head>
+    <title>Probando CodeSandBox</title>
+    <meta charset="UTF-8" />
+  </head>
+
+  <body>
+    <div>
+      <ul>
+        <li>Autor: <span id="info">David Rico</span></li>
+        <li>Contacto: dricomonzon@gmail.com</li>
+        <li>
+          Observaciones: Mis expectativas de este módulo son aprender a crear
+          funciones para simplificar el uso de apps.
+        </li>
+        <li>
+          Herramientas: Me gustaria aprender JavaScript, PHP, Python, Angular,
+          SQL... También me gustaría aprender a crear embudos de ventas en una
+          página web y otra cosa curiosa sería aprender a utilizar Amazon Web
+          Services para el despliegue de aplicaciones.
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
+`;
